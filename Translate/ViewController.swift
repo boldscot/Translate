38b0e2598bd8818d89c70ae149568065175b7923
20061696 <20061696@it103imac05.local>
2016-11-03 15:54:44 +0000

    Implemented picker system for language selection.

--- Translate/ViewController.swift
+++ Translate/ViewController.swift
@@ -3,24 +3,34 @@
 //  Translate
 //
 //  Created by Robert O'Connor on 16/10/2015.
+//  Edited by Stephen Collins on 3/11/2016
 //  Copyright © 2015 WIT. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UITextViewDelegate {
+class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var textToTranslate: UITextView!
     @IBOutlet weak var translatedText: UITextView!
+    @IBOutlet weak var languagePicker: UIPickerView!
+    
+    let languagePickerData = ["English", "French", "Turkish", "Gaelic"]
+    var languages: String = ""
+    var source: String = "en"
+    var dest: String = "en"
     
     //var data = NSMutableData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textToTranslate.delegate = self
+        textToTranslate.delegate = self         // set text view delegate as view controller
+        languagePicker.delegate = self
+        languagePicker.dataSource = self
     }
     
+    // This function resigns the first responder when the return key is pressed
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         if (text == "\n") {
             textToTranslate.resignFirstResponder()
@@ -34,12 +44,56 @@ class ViewController: UIViewController, UITextViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    // Number of columns
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return languagePickerData.count
+    }
+    
+    // Get data for specific row and column
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return languagePickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        if (component == 0) {
+            if (row == 0 ) {
+                source = "en"
+            } else if (row == 1) {
+                source = "fr"
+            } else if (row == 2) {
+                source = "tr"
+            } else if (row == 3) {
+                source = "ga"
+            }
+        } else if (component == 1) {
+            if (row == 0) {
+                dest = "en"
+            } else if (row == 1) {
+                dest = "fr"
+            } else if (row == 2) {
+                dest = "tr"
+            } else if (row == 3) {
+                dest = "ga"
+            }
+        }
+        
+        languages = source + "|" + dest
+        print("languages= " + languages)
+    }
+    
+    
     @IBAction func translate(_ sender: AnyObject) {
         
         let str = textToTranslate.text
         let escapedStr = str?.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
         
-        let langStr = ("en|fr").addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+        let langStr = (languages).addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
         
         let urlStr:String = ("https://api.mymemory.translated.net/get?q="+escapedStr!+"&langpair="+langStr!)
         
