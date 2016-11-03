2c287a53a51b9216073612132455592c99072dc1
20061696 <20061696@it103imac05.local>
2016-11-03 18:26:27 +0000

    removed code for attempted deprecated function fix

--- Translate/ViewController.swift
+++ Translate/ViewController.swift
@@ -83,10 +83,12 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
         let str = textToTranslate.text
         let escapedStr = str?.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
         let langStr = (languages).addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
-        
+
         let urlStr:String = ("https://api.mymemory.translated.net/get?q="+escapedStr!+"&langpair="+langStr!)
         let url = URL(string: urlStr)
         let request = URLRequest(url: url!)     // Creating Http Request
+        
+        let session = URLSession.shared
         
         
         //var data = NSMutableData()var data = NSMutableData()
@@ -98,27 +100,8 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
         
         var result = "<Translation Error>"
         
-       /* NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.main) { response, data, error in
+        NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.main) { response, data, error in
             
-            indicator.stopAnimating()
-            
-            if let httpResponse = response as? HTTPURLResponse {
-                if(httpResponse.statusCode == 200){
-                    
-                    let jsonDict: NSDictionary!=(try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)) as! NSDictionary
-                    
-                    if(jsonDict.value(forKey: "responseStatus") as! NSNumber == 200){
-                        let responseData: NSDictionary = jsonDict.object(forKey: "responseData") as! NSDictionary
-                        
-                        result = responseData.object(forKey: "translatedText") as! String
-                    }
-                }
-                
-                self.translatedText.text = result
-            }
-        } */
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
             indicator.stopAnimating()
             
             if let httpResponse = response as? HTTPURLResponse {
@@ -136,9 +119,6 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
                 self.translatedText.text = result
             }
         }
-        task.resume()
-        
-    }
 }
 
 
